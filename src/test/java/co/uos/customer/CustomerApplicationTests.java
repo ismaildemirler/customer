0a845d33ac26939d8054af226b1bb4c427479645
İsmail Demirler <isdem86@gmail.com>
2024-05-14 19:02:32 +0100

    First Commit

---
+++ src/test/java/co/uos/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uos.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
